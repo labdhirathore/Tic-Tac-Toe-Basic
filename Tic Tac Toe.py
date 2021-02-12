410f7c3a0e44ddb935db3e4c096512faf8bf6f60
Labdhi Rathore <61301997+labdhirathore@users.noreply.github.com>
2021-02-12 18:34:26 +0530

    Tic-Tac-Toe Game

---
+++ Tic Tac Toe.py
@@ -0,0 +1,239 @@
+# Import of Tkinter module
+from tkinter import *
+from tkinter import messagebox
+
+# Creates the window from the imported Tkinter module
+root = Tk()
+# Adds a title to the Windows GUI for the window
+root.title("Tic-Tac-Toe")
+# Loops the window to prevent the window from just "flashing once"
+# root.geometry("600x400")
+
+# X starts so true
+clicked = True
+count = 0
+
+# to disable all buttons
+
+
+def disable_all_buttons():
+    b1.config(state=DISABLED)
+    b2.config(state=DISABLED)
+    b3.config(state=DISABLED)
+    b4.config(state=DISABLED)
+    b5.config(state=DISABLED)
+    b6.config(state=DISABLED)
+    b7.config(state=DISABLED)
+    b8.config(state=DISABLED)
+    b9.config(state=DISABLED)
+
+
+# Checked to see if someone won
+
+
+def check_if_won():
+    global winner
+    winner = False
+    if b1["text"] == "X" and b2["text"] == "X" and b3["text"] == "X":
+        b1.config(bg="red")
+        b2.config(bg="red")
+        b3.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! X wins!")
+        disable_all_buttons()
+    elif b4["text"] == "X" and b5["text"] == "X" and b6["text"] == "X":
+        b4.config(bg="red")
+        b5.config(bg="red")
+        b6.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! X wins!")
+        disable_all_buttons()
+    elif b7["text"] == "X" and b8["text"] == "X" and b9["text"] == "X":
+        b7.config(bg="red")
+        b8.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! X wins!")
+        disable_all_buttons()
+    elif b1["text"] == "X" and b5["text"] == "X" and b9["text"] == "X":
+        b1.config(bg="red")
+        b5.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! X wins!")
+        disable_all_buttons()
+    elif b1["text"] == "X" and b4["text"] == "X" and b7["text"] == "X":
+        b1.config(bg="red")
+        b4.config(bg="red")
+        b7.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! X wins!")
+        disable_all_buttons()
+    elif b2["text"] == "X" and b5["text"] == "X" and b8["text"] == "X":
+        b2.config(bg="red")
+        b5.config(bg="red")
+        b8.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! X wins!")
+        disable_all_buttons()
+    elif b3["text"] == "X" and b6["text"] == "X" and b9["text"] == "X":
+        b3.config(bg="red")
+        b6.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! X wins!")
+        disable_all_buttons()
+    elif b3["text"] == "X" and b5["text"] == "X" and b7["text"] == "X":
+        b3.config(bg="red")
+        b5.config(bg="red")
+        b7.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! X wins!")
+        disable_all_buttons()
+
+    # Check for O
+    elif b1["text"] == "O" and b2["text"] == "O" and b3["text"] == "0":
+        b1.config(bg="red")
+        b2.config(bg="red")
+        b3.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! O wins!")
+        disable_all_buttons()
+    elif b4["text"] == "O" and b5["text"] == "O" and b6["text"] == "O":
+        b4.config(bg="red")
+        b5.config(bg="red")
+        b6.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! O wins!")
+        disable_all_buttons()
+    elif b7["text"] == "O" and b8["text"] == "O" and b9["text"] == "O":
+        b7.config(bg="red")
+        b8.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! O wins!")
+        disable_all_buttons()
+    elif b1["text"] == "O" and b5["text"] == "O" and b9["text"] == "O":
+        b1.config(bg="red")
+        b5.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! O wins!")
+        disable_all_buttons()
+    elif b1["text"] == "O" and b4["text"] == "O" and b7["text"] == "O":
+        b1.config(bg="red")
+        b4.config(bg="red")
+        b7.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! O wins!")
+        disable_all_buttons()
+    elif b2["text"] == "O" and b5["text"] == "O" and b8["text"] == "O":
+        b2.config(bg="red")
+        b5.config(bg="red")
+        b8.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! O wins!")
+        disable_all_buttons()
+    elif b3["text"] == "O" and b6["text"] == "O" and b9["text"] == "O":
+        b3.config(bg="red")
+        b6.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! O wins!")
+        disable_all_buttons()
+    elif b3["text"] == "O" and b5["text"] == "O" and b7["text"] == "O":
+        b3.config(bg="red")
+        b5.config(bg="red")
+        b7.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "Congratulations! O wins!")
+        disable_all_buttons()
+    # Check if tie
+    if count == 9 and winner is False:
+        messagebox.showinfo("Tic Tac Toe", "It a Tie!\n No one wins")
+        disable_all_buttons()
+
+
+# Button clicked function
+
+
+def b_click(b):
+    # pass
+    global clicked, count
+
+    if b["text"] == " " and clicked is True:
+        b["text"] = "X"
+        clicked = False
+        count += 1
+        check_if_won()
+
+    elif b["text"] == " " and clicked is False:
+        b["text"] = "O"
+        clicked = True
+        count += 1
+        check_if_won()
+    else:
+        messagebox.showerror("Tic Tac Toe", "Hey that box has been already been selected\nPick another box...")
+
+# reset the game
+
+
+def reset():
+    # Build our buttons
+    global b1, b2, b3, b4, b5, b6, b7, b8, b9
+    global clicked, count
+    clicked = True
+    count = 0
+
+    b1 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6,
+                bg="SystemButtonFace", command=lambda: b_click(b1))
+    b2 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6,
+                bg="SystemButtonFace", command=lambda: b_click(b2))
+    b3 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6,
+                bg="SystemButtonFace", command=lambda: b_click(b3))
+
+    b4 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6,
+                bg="SystemButtonFace", command=lambda: b_click(b4))
+    b5 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6,
+                bg="SystemButtonFace", command=lambda: b_click(b5))
+    b6 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6,
+                bg="SystemButtonFace", command=lambda: b_click(b6))
+
+    b7 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6,
+                bg="SystemButtonFace", command=lambda: b_click(b7))
+    b8 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6,
+                bg="SystemButtonFace", command=lambda: b_click(b8))
+    b9 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6,
+                bg="SystemButtonFace", command=lambda: b_click(b9))
+
+    # Grid our buttons to the screen
+    b1.grid(row=0, column=0)
+    b2.grid(row=0, column=1)
+    b3.grid(row=0, column=2)
+
+    b4.grid(row=1, column=0)
+    b5.grid(row=1, column=1)
+    b6.grid(row=1, column=2)
+
+    b7.grid(row=2, column=0)
+    b8.grid(row=2, column=1)
+    b9.grid(row=2, column=2)
+
+# Create Menu
+
+
+my_menu = Menu(root)
+# Tkinter know how a menu looks and does all the work in background we don't have to define it
+root.config(menu=my_menu)
+
+# Let's create options menu
+options_menu = Menu(my_menu, tearoff=False)  # tearoff for no dotted lines
+my_menu.add_cascade(label="Options", menu=options_menu)  # Creating submenus
+options_menu.add_command(label="Reset Game", command=reset)  # Adding things to submenu
+# Separating diff options by a bar
+options_menu.add_separator()
+options_menu.add_command(label="Quit Game", command=root.quit)
+reset()
+
+# Loops the window to prevent the window from just "flashing once"
+root.mainloop()
